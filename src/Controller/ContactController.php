321855e264991e6bbffcd49dc6b04116af038702
hugo613 <hugo.getin@gmail.com>
2023-01-07 18:54:40 +0100

    contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,19 +5,40 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Repository\ContactRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/admin/contact', name: 'app_contact_admin')]
+    public function index(ContactRepository $contactRepository): Response
     {
-        $form=$this->createForm(ContactType::class);
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-            'formulaire' => $form
+        $tab = $contactRepository->findAll();
+        return $this->render('contact/index.html.twig', ['lstContact' => $tab]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function sendMessage(HttpFoundationRequest $request, EntityManagerInterface $entityManager): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $contact->setDate(new DateTimeImmutable());
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            $this->addFlash('success', 'Categorie cree avec succes !');
+            return $this->redirectToRoute('app_contact');
+        
+        }
+        return $this->render('contact/form.html.twig', [
+            'formView' => $form->createView()
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,16 +15,16 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('demande')
-            ->add('Envoyer', SubmitType::class)
+            ->add('mail', TextType::class, ['attr'=>['class'=>'form-control', 'placeholder'=>'Saisir email']])
+            ->add('description', TextareaType::class, ['label'=>'Demande', 'attr'=>['class'=>'form-control', 'placeholder'=>'Saisir votre demande']])
+            ->add('add', SubmitType::class, ['label'=>'Envoyer message', 'attr'=>['class'=>'form-control sumbit-form']])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Contact::class,
         ]);
     }
 }
